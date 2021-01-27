fc713f94e3c10865886f3e02bd4b91dd91c95d2f
Aditi Ohri <didiohri@gmail.com>
2021-01-26 19:12:06 -0600

    first draft of answer for minimum done

--- functions/minimum.js
+++ functions/minimum.js
@@ -1,1 +1,9 @@
 /** write  a function, min, that takes two arguments and returns their minimum */
+
+function min(a, b) {
+  return a > b ? b : a;
+}
+
+console.log(min(3, 4))
+console.log(min(33, 409))
+console.log(min("three", "four"))
