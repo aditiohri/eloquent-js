74ff794555fe9af07a63072e53dcef3ad9f0e8d4
Aditi Ohri <didiohri@gmail.com>
2021-01-26 19:30:00 -0600

    write countChar and call from countBs

--- functions/beanCounting.js
+++ functions/beanCounting.js
@@ -1,11 +1,7 @@
 /** write a function, countBs, that takes a string as its only argument and returns a number that indicates how many uppercase "B" characters there are in the string */
 
 function countBs(str) {
-    let count = 0;
-    for (let i = 0; i < str.length; i++) {
-        if (str[i] === "B") count++
-    }
-    return count;
+    return countChar(str, "B")
 }
 
 console.log(countBs("BumbleBees")) //2
@@ -13,3 +9,11 @@ console.log(countBs("wedidit")) //0
 console.log(countBs("suBpar")) //1
 
 /** write a function, countChar, that behaves like countBs except it takes a second argument that indicates the character that is to be counted rather than only uppercase "B" characters. rewrite countBs to make use of this new function */
+
+function countChar(str, char) {
+    let count = 0;
+    for (let i = 0; i < str.length; i++) {
+        if (str[i] === char) count++
+    }
+    return count;
+}
