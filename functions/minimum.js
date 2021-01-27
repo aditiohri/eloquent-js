9f65b41c38ad19ba17497933f67ded558b26ffb7
Aditi Ohri <didiohri@gmail.com>
2021-01-26 19:17:03 -0600

    add type coercion and testing for number types in minimum.js

--- functions/minimum.js
+++ functions/minimum.js
@@ -1,9 +1,11 @@
 /** write  a function, min, that takes two arguments and returns their minimum */
 
 function min(a, b) {
+  if (Number.isNaN(Number(a)) || Number.isNaN(Number(a)))
+    return "Only numbers allowed as arguments";
   return a > b ? b : a;
 }
 
-console.log(min(3, 4))
-console.log(min(33, 409))
-console.log(min("three", "four"))
+console.log(min(3, 4));
+console.log(min(33, 409));
+console.log(min("three", "four"));
