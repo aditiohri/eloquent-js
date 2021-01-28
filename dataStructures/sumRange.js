a4678d41a165ca2470ffa4e9525c4b3778b10ada
Aditi Ohri <didiohri@gmail.com>
2021-01-27 21:22:17 -0600

    range can now accept a step argument

--- dataStructures/sumRange.js
+++ dataStructures/sumRange.js
@@ -5,22 +5,22 @@
  * last, modify your range function to take an optional third argument that indicates the "step" value used when building the array. if no step is given, the elements go up by increments of one, as before. the function call range(1, 10, 2) should return [1, 3, 5, 7, 9]. it should also work with negative step values so that range(5, 2, -1) returns [5,4,3,2]
  */
 
-function range(start, end) {
-    let nums = [start];
-    for (let i = 1; i <= (end-start); i++) {
-        nums.push(start+i);
-    }
-    return nums
+function range(start, end, step = 1) {
+  let nums = [start];
+  for (let i = 1; start+(step*i) <= end; i++) {
+      nums.push(start + step * i)
+  }
+  return nums;
 }
 
-console.log(range(1, 100))
+console.log(range(5, 2, -1));
 
 function sum([...numbers]) {
-    let total = 0;
-    for (let number of numbers) {
-        total += number;
-    }
-    return total;
+  let total = 0;
+  for (let number of numbers) {
+    total += number;
+  }
+  return total;
 }
 
-console.log(sum(range(1, 10)))
+// console.log(sum(range(1, 10)));
