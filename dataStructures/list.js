d03dcb2c66f6d802d18225b587c643550fad32cd
Aditi Ohri <didiohri@gmail.com>
2021-01-28 21:53:41 -0600

    objects are so cool, arrayToList done

---
+++ dataStructures/list.js
@@ -0,0 +1,21 @@
+/** let list = { value: 1, rest: { value: 2, rest: { value: 3, rest: null }}}
+ * write a function arrayToList that builds up a list structure like the one shown when given [1, 2, 3] as an argument.
+ * also write a listToArray function that produces an array from a list.
+ * then add a helper function prepend, which takes an element and a list and creates a new list that adds the element to the front of the input list,
+ * and nth, a function that takes a list and a number and retuns the element at the given position in the list or undefined when there is no such element.
+ * write a recursive version of nth as well.
+ */
+
+// let list = { value: 1, rest: { value: 2, rest: { value: 3, rest: null }}}
+
+// console.log(list[1])
+
+function arrayToList(arr) {
+  let list = null;
+  for (let i = arr.length - 1; i >= 0; i--) {
+      list = { value: arr[i], rest: list }
+  }
+  return list;
+}
+
+console.log(arrayToList([1, 2, 3]));
